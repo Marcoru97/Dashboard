5a3400877a3d3cd0a1bcbbe5c242797d90a4df63
Marco Rußinski <Marco.Russinski@outlook.de>
2018-06-25 01:24:36 +0200

    Add button component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <icon-button icon="settings"></icon-button>
     <overview></overview>
   </div>
 </template>
 
 <script>
 import Overview from './components/dashboard_grid/Grid';
+import IconButton from './components/dashboard_button/IconButton';
 
 export default {
   name: 'dashboard',
   components: {
     Overview,
+    IconButton,
   },
 };
 </script>
---
+++ src/renderer/components/dashboard_button/IconButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button>
+    <svg>
+      <use xlink:href="assets/settings.svg#settings" />
+    </svg>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'icon-button',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" src="./styles.scss" scoped />
