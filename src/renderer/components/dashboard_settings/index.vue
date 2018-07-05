ad96f850a52c5a1b94360171e789270793a9e4ba
Marco Rußinski <Marco.Russinski@outlook.de>
2018-07-05 23:06:57 +0200

    Fix settings arrow icon

--- src/renderer/components/dashboard_settings/index.vue
+++ src/renderer/components/dashboard_settings/index.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="dashboard-settings__wrapper">
-        <transition name="dashboard-settings__menu-animation">
-        <div class="dashboard-settings__menu" v-show="settingsVisible">
-            <section class="dasboard-settings-menu__header">
-                <h1 class="dashboard-settings-menu__header">Settings</h1>
-                <icon-button
-                  icon="arrow_right"
-                  icon-color="light-gray"
-                  class="dashboard-settings-menu__toggle-button"
-                  @click="$emit('closeSettings')"
-                />
-            </section>
-            <!-- <hr class="dashboard-settings-menu-line" /> -->
-        </div>
-        </transition>
-    </div>
+  <div class="dashboard-settings__wrapper">
+    <transition name="dashboard-settings__menu-animation">
+      <div class="dashboard-settings__menu" v-show="settingsVisible">
+        <section class="dasboard-settings-menu__header">
+          <h1 class="dashboard-settings-menu__header">Settings</h1>
+          <!-- TODO: The icon behaviour is really strange -->
+          <icon-button
+            icon="arrow_right"
+            :inline-icon-style="{width: '100%', height: '75%'}"
+            class="dashboard-settings-menu__toggle-button"
+            @click="$emit('closeSettings')"
+          />
+        </section>
+        <!-- <hr class="dashboard-settings-menu-line" /> -->
+      </div>
+    </transition>
+  </div>
 </template>
 <script>
 import IconButton from './../dashboard_button';
