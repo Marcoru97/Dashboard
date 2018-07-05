e7f8a2e24e447dc72196297cb786cb88fe5f8ea2
Marco Rußinski <Marco.Russinski@outlook.de>
2018-07-05 21:27:07 +0200

    Fix typo

--- src/renderer/components/dashboard_settings/index.vue
+++ src/renderer/components/dashboard_settings/index.vue
@@ -3,7 +3,7 @@
         <transition name="dashboard-settings__menu-animation">
         <div class="dashboard-settings__menu" v-show="settingsVisible">
             <section class="dasboard-settings-menu__header">
-                <h1 class="dashbpard-settings-menu__header">Settings</h1>
+                <h1 class="dashboard-settings-menu__header">Settings</h1>
                 <icon-button
                   icon="arrow_right"
                   icon-color="light-gray"
