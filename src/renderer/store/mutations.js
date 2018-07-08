f5ab681dc570227516c6838dbd2134363a1a9244
Marco Rußinski <Marco.Russinski@outlook.de>
2018-07-08 02:23:38 +0200

    Fix mutation

--- src/renderer/store/mutations.js
+++ src/renderer/store/mutations.js
@@ -2,13 +2,14 @@ import types from './types';
 
 export default {
   [types.mutations.ITEM_DATA_ADD](state, data) {
-    state.itemData.push(
-      {
-        position: 0,
+    state.itemData.push({
+      ...{
+        position: state.itemId,
         name: '',
         size: { width: 1, height: 1 },
       },
       ...data,
-    );
+    });
+    state.itemId++;
   },
 };
