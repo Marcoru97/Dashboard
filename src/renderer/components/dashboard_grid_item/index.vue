71322f7bec9d4d7088ce21653cbaccc59f1046e4
Marco_ru <marco.russi97@gmail.com>
2019-02-17 23:27:20 +0100

    WIP Add basic grid layout

--- src/renderer/components/dashboard_grid_item/index.vue
+++ src/renderer/components/dashboard_grid_item/index.vue
@@ -1,127 +1,48 @@
 <template>
-  <div class="dashboard-item">
-    <error-message
-      v-show="extensionHasError"
-      errorMessageSimple="Something went wrong while loading the extension!"
-      :errorMessage="extensionErrorMessage"
-    />
-    <div class="dasboard-item__extension" v-html="extensionTemplate"></div>
+  <div class="dashboard-item" :style="style">
+    <slot/>
   </div>
 </template>
 
 <script>
-import path from 'path';
-import fs from 'fs';
-
-import ErrorMessage from '../dashboard_error';
+import { mapState } from 'vuex';
 
 export default {
   name: 'grid_item',
 
   props: {
-    extension: {
-      type: String,
+    size: {
+      type: Object,
       required: true,
     },
-    extensionSize: {
+    position: {
       type: Object,
       required: true,
     },
   },
 
-  components: {
-    ErrorMessage,
-  },
-
   data() {
-    return {
-      extensionErrorMessage: '',
-      extensionHasError: false,
-      extensionLoaded: false,
-      extensionData: {
-        name: 'New Extension',
-        description: 'No description :(',
-        version: '-',
-        author: '',
-      },
-      extensionJavascript: {},
-      extensionTemplate: '',
-      extensionPath: path.join(this.$store.state.appDir, 'extensions', this.extension),
-    };
+    return {};
   },
 
-  mounted() {
-    this.loadExtensionFiles()
-      .then(() => {
-        if (Object.prototype.hasOwnProperty.call(this.extensionJavascript, 'loaded')) {
-          this.extensionJavascript.loaded();
-        }
-      })
-      .catch((error) => {
-        this.showConsoleError(error);
-        this.extensionErrorMessage = error;
-        this.extensionHasError = true;
-      });
-  },
+  mounted() {},
 
-  methods: {
-    loadExtensionFiles() {
-      return new Promise(async (resolve, reject) => {
-        try {
-          // Loads the json config file
-          const configPath = path.join(this.extensionPath, 'extension.json');
-          const jsonFileData = this.loadFile(configPath);
+  computed: {
+    ...mapState({
+      margin: state => state.settings.itemMargin,
+      itemSize: 'itemSize',
+    }),
 
-          this.extensionData = {
-            ...JSON.parse(jsonFileData),
-            ...this.extensionData,
-          };
+    style() {
+      const top = this.margin * (this.position.x + 1) + this.itemSize * this.position.x;
+      const left = this.margin * (this.position.y + 1) + this.itemSize * this.position.y;
 
-          // Loads the javascript file
-          if (this.extensionData.js) {
-            const javascriptPath = path.join(this.extensionPath, this.extensionData.js);
-            const javascriptFileData = this.loadFile(javascriptPath);
-
-            // eslint-disable-next-line no-new-func
-            this.extensionJavascript = new Function(
-              'element',
-              'itemSize',
-              `"use strict"; ${javascriptFileData}`,
-            )(this.$el, this.extensionSize);
-
-            if (Object.prototype.hasOwnProperty.call(this.extensionJavascript, 'initialize')) {
-              this.extensionJavascript.initialize();
-            }
-          }
-
-          // Loads the html file
-          if (this.extensionData.html) {
-            const htmlPath = path.join(this.extensionPath, this.extensionData.html);
-            this.extensionTemplate = this.loadFile(htmlPath);
-          }
-
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      });
-    },
-
-    loadFile(filePath) {
-      if (fs.existsSync(filePath)) {
-        return fs.readFileSync(filePath, 'utf8');
-      }
-      throw new Error(`File not found! (${filePath})`);
-    },
-
-    showConsoleError(error) {
-      // TODO: Maybe outsource it and make it more generic?
-      // eslint-disable-next-line no-console
-      console.log(
-        'An error occurred while loading the extension \n',
-        `Extension: '${this.extension}' \n`,
-        `Error: ${error} \n`,
-      );
+      return {
+        top: `${top}px`,
+        left: `${left}px`,
+        width: `${this.size.width * this.itemSize}px`,
+        height: `${this.size.height * this.itemSize}px`,
+      };
     },
   },
 };
--- src/renderer/components/dashboard_overview/index.vue
+++ src/renderer/components/dashboard_overview/index.vue
@@ -2,19 +2,25 @@
   <div>
     <grid>
       <grid-item
-        v-for="(itemModule, key) in getModulesFromCurrentTab()"
+        v-for="(extension, key) in extensions"
         :key="key"
-        :extension="itemModule.module"
-        :extensionSize="itemModule.size"
-      />
+        :size="extension.size"
+        :max-size="maxItemSize"
+        :position="extension.position"
+      >
+        <extension :name="extension.module" :size="extension.size"/>
+      </grid-item>
     </grid>
   </div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapState } from 'vuex';
+import { listen } from 'popmotion';
+import throttle from 'lodash.throttle';
 
 import Grid from '../dashboard_grid';
 import GridItem from '../dashboard_grid_item';
+import Extension from '../dashboard_extension';
 
 export default {
   name: 'overview',
@@ -22,17 +28,47 @@ export default {
   components: {
     Grid,
     GridItem,
+    Extension,
   },
 
   data() {
-    return {};
+    return {
+      windowWidth: 0,
+      resizeListener: null,
+    };
   },
 
-  methods: {
-    ...mapGetters(['getModulesFromCurrentTab']),
+  mounted() {
+    this.windowWidth = window.innerWidth;
+    this.resizeListener = listen(window, 'resize').start(
+      throttle(() => {
+        this.windowWidth = window.innerWidth;
+      }, 66),
+    );
   },
 
-  computed: {},
+  destroyed() {
+    this.resizeListener.stop();
+  },
+
+  computed: {
+    ...mapGetters({
+      extensions: 'getModulesFromCurrentTab',
+    }),
+
+    ...mapState({
+      itemMargin: state => state.settings.itemMargin,
+      itemSize: 'itemSize',
+    }),
+
+    maxItemSize() {
+      const maxSize = Math.floor(
+        (this.windowWidth - this.itemMargin) / (this.itemMargin + this.itemSize),
+      );
+
+      return Math.max(maxSize, 1);
+    },
+  },
 };
 </script>
 <style lang="scss" src="./styles.scss" scoped />
